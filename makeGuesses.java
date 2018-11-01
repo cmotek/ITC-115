98f3aabb13e0735c8a7bb1a0bd2f60d59082e02e
flowzeliak <38844285+flowzeliak@users.noreply.github.com>
2018-10-31 21:35:11 -0700

    Fixed Assignment 5

--- makeGuesses.java
+++ makeGuesses.java
@@ -1,19 +1,21 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class makeGuesses {
+public class guess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner console = new Scanner(System.in);
-		int max = 50;
-		int min = 1;
-		int Guess = (int)Math.random();
-		
-		while(Guess <= 48) {
-			System.out.print(Guess + ",");
-			Guess = console.nextInt();
+		Random r = new Random();
+		int num = 0;
+		int sum = 0;
+		while(num <= 48) {
+			num = r.nextInt(51);
+			System.out.println("guess = " + num);
+			sum += 1;
 		}
-
+		System.out.println("total guesses = " + sum);
 	}
 
 }
+
